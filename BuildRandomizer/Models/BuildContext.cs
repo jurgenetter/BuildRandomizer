fb8f65a54939ebe8ee919a9d854aba5da8c61f13
Jacob Etter <jurgenetter@gmail.com>
2018-01-04 13:15:55 -0700

    First Commit

--- BuildRandomizer/Models/BuildContext.cs
+++ BuildRandomizer/Models/BuildContext.cs
@@ -23,6 +23,7 @@ namespace BuildRandomizer.Models
 		public DbSet<SkillGem> SkillGems { get; set; }
 		public DbSet<WeaponType> WeaponTypes { get; set; }
 
+
 		// Add a DbSet for each entity type that you want to include in your model. For more information 
 		// on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
